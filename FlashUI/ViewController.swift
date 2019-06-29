4ce1f266c109b0cc8aa285d6f01f5f78d8784c06
Tsuf <tifatsubasa@gmail.com>
2019-06-30 02:08:38 +0800

    Initial Commit

---
+++ FlashUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashUI
+//
+//  Created by tsuf on 2019/6/30.
+//  Copyright © 2019 upmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
