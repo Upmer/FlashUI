80ad40a25246ba69ae8390fb413d046fc68a200a
Tsuf <tifatsubasa@gmail.com>
2019-07-06 00:09:54 +0800

    feat: complete image view

--- FlashUI/Source/UIImageView+FlashUI.swift
+++ FlashUI/Source/UIImageView+FlashUI.swift
@@ -13,4 +13,30 @@ extension UIImageView {
     self.image = image
     return self
   }
+  func highlightedImage(_ highlightedImage: UIImage?) -> Self {
+    self.highlightedImage = highlightedImage
+    return self
+  }
+  func isHighlighted(_ isHighlighted: Bool) -> Self {
+    self.isHighlighted = isHighlighted
+    return self
+  }
+  
+  func animationImages(_ animationImages: [UIImage]?) -> Self {
+    self.animationImages = animationImages
+    return self
+  }
+  func highlightedAnimationImages(_ highlightedAnimationImages: [UIImage]?) -> Self {
+    self.highlightedAnimationImages = highlightedAnimationImages
+    return self
+  }
+  
+  func animationDuration(_ animationDuration: TimeInterval) -> Self {
+    self.animationDuration = animationDuration
+    return self
+  }
+  func animationRepeatCount(_ animationRepeatCount: Int) -> Self {
+    self.animationRepeatCount = animationRepeatCount
+    return self
+  }
 }
