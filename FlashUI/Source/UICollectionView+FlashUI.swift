4df84b4d8ad955eb508987bd2e79049808a9a583
Tsuf <tifatsubasa@gmail.com>
2019-07-06 00:57:32 +0800

    feat: complete collection view

--- FlashUI/Source/UICollectionView+FlashUI.swift
+++ FlashUI/Source/UICollectionView+FlashUI.swift
@@ -17,4 +17,61 @@ extension UICollectionView {
     self.delegate = delegate
     return self
   }
+  
+  @available(iOS 10.0, *)
+  weak func prefetchDataSource(_ prefetchDataSource: UICollectionViewDataSourcePrefetching?) -> Self {
+    self.prefetchDataSource = prefetchDataSource
+    return self
+  }
+  
+  @available(iOS 10.0, *)
+  func isPrefetchingEnabled(_ isPrefetchingEnabled: Bool) -> Self {
+    self.isPrefetchingEnabled = isPrefetchingEnabled
+    return self
+  }
+  
+  @available(iOS 11.0, *)
+  func dragDelegate(_ dragDelegate: UICollectionViewDragDelegate?) -> Self {
+    self.dragDelegate = dragDelegate
+    return self
+  }
+  
+  @available(iOS 11.0, *)
+  func dropDelegate(_ dropDelegate: UICollectionViewDropDelegate?) -> Self {
+    self.dropDelegate = dropDelegate
+    return self
+  }
+  
+  @available(iOS 11.0, *)
+  func dragInteractionEnabled(_ dragInteractionEnabled: Bool) -> Self {
+    self.dragInteractionEnabled = dragInteractionEnabled
+    return self
+  }
+  
+  @available(iOS 11.0, *)
+  func reorderingCadence(_ reorderingCadence: UICollectionView.ReorderingCadence) -> Self {
+    self.reorderingCadence = reorderingCadence
+    return self
+  }
+  
+  func backgroundView(_ backgroundView: UIView?) -> Self {
+    self.backgroundView = backgroundView
+    return self
+  }
+  
+  func allowsSelection(_ allowsSelection: Bool) -> Self {
+    self.allowsSelection = allowsSelection
+    return self
+  }
+  func allowsMultipleSelection(_ allowsMultipleSelection: Bool) -> Self {
+    self.allowsMultipleSelection = allowsMultipleSelection
+    return self
+  }
+  
+  // Support for Focus
+  @available(iOS 9.0, *)
+  func remembersLastFocusedIndexPath(_ remembersLastFocusedIndexPath: Bool) -> Self {
+    self.remembersLastFocusedIndexPath = remembersLastFocusedIndexPath
+    return self
+  }
 }
