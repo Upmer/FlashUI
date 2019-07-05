62252da219459cfd615147c1bf44fe0543faaebb
Tsuf <tifatsubasa@gmail.com>
2019-07-06 00:25:32 +0800

    feat: complete text field

--- FlashUI/Source/UITextField+FlashUI.swift
+++ FlashUI/Source/UITextField+FlashUI.swift
@@ -37,4 +37,83 @@ extension UITextField {
     self.delegate = delegate
     return self
   }
+  
+  func attributedText(_ attributedText: NSAttributedString?) -> Self {
+    self.attributedText = attributedText
+    return self
+  }
+  func defaultTextAttributes(_ defaultTextAttributes: [NSAttributedString.Key : Any]) -> Self {
+    self.defaultTextAttributes = defaultTextAttributes
+    return self
+  }
+  func attributedPlaceholder(_ attributedPlaceholder: NSAttributedString?) -> Self {
+    self.attributedPlaceholder = attributedPlaceholder
+    return self
+  }
+  
+  func clearsOnBeginEditing(_ clearsOnBeginEditing: Bool) -> Self {
+    self.clearsOnBeginEditing = clearsOnBeginEditing
+    return self
+  }
+  func adjustsFontSizeToFitWidth(_ adjustsFontSizeToFitWidth: Bool) -> Self {
+    self.adjustsFontSizeToFitWidth = adjustsFontSizeToFitWidth
+    return self
+  }
+  func minimumFontSize(_ minimumFontSize: CGFloat) -> Self {
+    self.minimumFontSize = minimumFontSize
+    return self
+  }
+
+  func background(_ background: UIImage?) -> Self {
+    self.background = background
+    return self
+  }
+  func disabledBackground(_ disabledBackground: UIImage?) -> Self {
+    self.disabledBackground = disabledBackground
+    return self
+  }
+  func allowsEditingTextAttributes(_ allowsEditingTextAttributes: Bool) -> Self {
+    self.allowsEditingTextAttributes = allowsEditingTextAttributes
+    return self
+  }
+  func typingAttributes(_ typingAttributes: [NSAttributedString.Key : Any]?) -> Self {
+    self.typingAttributes = typingAttributes
+    return self
+  }
+  
+  func clearButtonMode(_ clearButtonMode: UITextField.ViewMode) -> Self {
+    self.clearButtonMode = clearButtonMode
+    return self
+  }
+  
+  func leftViewMode(_ leftViewMode: UITextField.ViewMode) -> Self {
+    self.leftViewMode = leftViewMode
+    return self
+  }
+  
+  
+  func rightView(_ rightView: UIView?) -> Self {
+    self.rightView = rightView
+    return self
+  }
+  
+  func rightViewMode(_ rightViewMode: UITextField.ViewMode) -> Self {
+    self.rightViewMode = rightViewMode
+    return self
+  }
+  
+  func inputView(_ inputView: UIView?) -> Self {
+    self.inputView = inputView
+    return self
+  }
+  
+  func inputAccessoryView(_ inputAccessoryView: UIView?) -> Self {
+    self.inputAccessoryView = inputAccessoryView
+    return self
+  }
+  
+  func clearsOnInsertion(_ clearsOnInsertion: Bool) -> Self {
+    self.clearsOnInsertion = clearsOnInsertion
+    return self
+  }
 }
